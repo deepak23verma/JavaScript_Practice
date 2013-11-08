a827f92776450b69c4a67b993f9cf71f5cc36952
Deepak Verma <deepak.23.verma@gmail.com>
2013-11-08 14:24:23 -0500

    Ex 2.3 - draw triangle

--- eloquent_js_ex.js
+++ eloquent_js_ex.js
@@ -1,7 +1,17 @@
+// Ex 2.2 - 2 to the 10th power
 var counter = 1;
 var total = 1;
 while (counter <= 10) {
   total = total * 2;
-  consol.log("2 to the power of: " + counter + " is: " + total);
+  console.log("2 to the power of: " + counter + " is: " + total);
   counter = counter + 1;
+}
+
+// Ex 2.3 - draw a triangle in 10 lines using "#"
+var line = ""
+var counter = 1
+while (counter <= 10) {
+  line = line + "#";
+  console.log(line);
+  counter += 1;
 }
