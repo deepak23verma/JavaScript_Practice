15941227989ee04e348db428de3fe1520f7d36d5
Deepak Verma <deepak.23.verma@gmail.com>
2013-11-08 14:41:01 -0500

    ex 2.4 and 2.5

--- eloquent_js_ex.js
+++ eloquent_js_ex.js
@@ -8,10 +8,24 @@ while (counter <= 10) {
 }
 
 // Ex 2.3 - draw a triangle in 10 lines using "#"
-var line = ""
-var counter = 1
+var line = "";
+var counter = 1;
 while (counter <= 10) {
   line = line + "#";
   console.log(line);
   counter += 1;
 }
+
+// Ex 2.4 rewrite 2.2 using a for loop
+var total = 1;
+for (var counter = 0; counter < 10; counter += 1)
+  total = total * 2;
+console.log(total);
+
+// Ex 2.5 rewrite 2.3 using a for loop
+var line = "";
+for (var counter = 0; counter < 10; counter += 1) {
+  line = line + "#";
+  console.log(line);
+};
+  
